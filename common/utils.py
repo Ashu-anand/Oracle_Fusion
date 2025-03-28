a53391bf678b6bb8cb6bdda573afbe870560c0d5
Ashu-anand <ashu.anand09@gmail.com>
2025-03-28 15:41:37 -0700

    Initial Code

---
+++ common/utils.py
@@ -0,0 +1,12 @@
+import base64
+from collections import defaultdict
+
+def generate_basic_auth_token(username: str,
+                              passowrd: str)->str:
+    credentials : str =f"{username}:{passowrd}"    
+    return f"Basic {base64.b64encode(credentials.encode()).decode()}"
+
+def conver_dict(dd):
+    if isinstance(dd,defaultdict):
+        return {k:conver_dict(v) for k,v in dd.items()}
+    return dd
