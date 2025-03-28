ae37b21ba6268a013b34e11a0a0a1d9b2db31fbe
Ashu-anand <ashu.anand09@gmail.com>
2025-03-28 15:46:29 -0700

    Fix

--- common/utils.py
+++ common/utils.py
@@ -6,7 +6,7 @@ def generate_basic_auth_token(username: str,
     credentials : str =f"{username}:{passowrd}"    
     return f"Basic {base64.b64encode(credentials.encode()).decode()}"
 
-def conver_dict(dd):
+def convert_dict(dd):
     if isinstance(dd,defaultdict):
         return {k:conver_dict(v) for k,v in dd.items()}
     return dd
